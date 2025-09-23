48a2b04db4eb6a2f66fdd9776576e4851267971d
TEJANAGADURGAVENKAT <tejutejaa117@gmail.com>
2025-09-23 11:15:02 +0530

    Fix merge conflicts in App.tsx - resolve all Git conflict markers

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,8 @@ import Navigation from "./components/layout/Navigation";
 import Footer from "./components/layout/Footer";
 import { AuthProvider } from "@/hooks/useAuth";
 import ProtectedRoute from "@/components/auth/ProtectedRoute";
-<<<<<<< HEAD
 import WorkerStatusGuard from "@/components/auth/WorkerStatusGuard";
 import InterviewStatusGuard from "@/components/auth/InterviewStatusGuard";
-=======
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
 
 // Auth pages
 import SignIn from "./pages/auth/SignIn";
@@ -33,24 +30,17 @@ import WorkerJobs from "./pages/worker/WorkerJobs";
 import WorkerTasks from "./pages/worker/WorkerTasks";
 import WorkerEarnings from "./pages/worker/WorkerEarnings";
 import WorkerProfile from "./pages/worker/WorkerProfile";
-<<<<<<< HEAD
 import DocumentUpload from "./pages/worker/onboarding/DocumentUpload";
 import TaskDetail from "./pages/worker/TaskDetail";
 import InterviewSchedule from "./pages/worker/InterviewSchedule";
 import TestRedirect from "./pages/debug/TestRedirect";
-=======
-import TaskDetail from "./pages/worker/TaskDetail";
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
 
 // Employer pages
 import EmployerDashboard from "./pages/employer/EmployerDashboard";
 import EmployerCampaigns from "./pages/employer/EmployerCampaigns";
 import CreateTask from "./pages/employer/CreateTask";
-<<<<<<< HEAD
 import WorkerVerification from "./pages/employer/WorkerVerification";
 import InterviewScheduling from "./pages/employer/InterviewScheduling";
-=======
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
 import SubmissionsReview from "./pages/employer/SubmissionsReview";
 import EmployerPayments from "./pages/employer/EmployerPayments";
 import EmployerTaskDetail from "./pages/employer/EmployerTaskDetail";
@@ -108,10 +98,7 @@ function App() {
                 <Route path="/forgot-password" element={<ForgotPassword />} />
 
                 {/* Worker Routes */}
-<<<<<<< HEAD
                 <Route path="/worker/onboarding/documents" element={<DocumentUpload />} />
-=======
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
                 <Route path="/worker" element={
                   <ProtectedRoute allowedRoles={['worker']}>
                     <WorkerDashboard />
@@ -119,35 +106,23 @@ function App() {
                 } />
                 <Route path="/worker/jobs" element={
                   <ProtectedRoute allowedRoles={['worker']}>
-<<<<<<< HEAD
                     <WorkerStatusGuard requiresJobAccess={true}>
                       <WorkerJobs />
                     </WorkerStatusGuard>
-=======
-                    <WorkerJobs />
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
                   </ProtectedRoute>
                 } />
                 <Route path="/worker/tasks" element={
                   <ProtectedRoute allowedRoles={['worker']}>
-<<<<<<< HEAD
                     <WorkerStatusGuard requiresTaskSubmission={true}>
                       <WorkerTasks />
                     </WorkerStatusGuard>
-=======
-                    <WorkerTasks />
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
                   </ProtectedRoute>
                 } />
                 <Route path="/worker/earnings" element={
                   <ProtectedRoute allowedRoles={['worker']}>
-<<<<<<< HEAD
                     <WorkerStatusGuard requiresTaskSubmission={true}>
                       <WorkerEarnings />
                     </WorkerStatusGuard>
-=======
-                    <WorkerEarnings />
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
                   </ProtectedRoute>
                 } />
                 <Route path="/worker/profile" element={
@@ -157,7 +132,6 @@ function App() {
                 } />
                 <Route path="/worker/task/:id" element={
                   <ProtectedRoute allowedRoles={['worker']}>
-<<<<<<< HEAD
                     <WorkerStatusGuard requiresTaskSubmission={true}>
                       <TaskDetail />
                     </WorkerStatusGuard>
@@ -171,11 +145,6 @@ function App() {
                   </ProtectedRoute>
                 } />
                 <Route path="/debug/test-redirect" element={<TestRedirect />} />
-=======
-                    <TaskDetail />
-                  </ProtectedRoute>
-                } />
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
 
                 {/* Employer Routes */}
                 <Route path="/employer" element={
@@ -198,7 +167,6 @@ function App() {
                     <RoleBasedTasks />
                   </ProtectedRoute>
                 } />
-<<<<<<< HEAD
                 <Route path="/employer/worker-verification" element={
                   <ProtectedRoute allowedRoles={['employer']}>
                     <WorkerVerification />
@@ -209,8 +177,6 @@ function App() {
                     <InterviewScheduling />
                   </ProtectedRoute>
                 } />
-=======
->>>>>>> 22b8ef5c22a983643de0cfb17e170083bc49eaa6
                 <Route path="/employer/submissions" element={
                   <ProtectedRoute allowedRoles={['employer']}>
                     <SubmissionsReview />
